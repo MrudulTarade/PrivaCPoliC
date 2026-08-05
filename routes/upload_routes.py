39dd796bd02326867d4d52ad8f32fdf309a9eb31
MrudulTarade <149925407+MrudulTarade@users.noreply.github.com>
2026-08-05 23:23:43 +0200

    foundation/chunking
    Propositional chunking algorithm applied for the preprocessed text.

--- routes/upload_routes.py
+++ routes/upload_routes.py
@@ -3,6 +3,8 @@ from flask import *
 from werkzeug.utils import secure_filename
 from services.pdf_reader import extract_text
 from services.preprocessing import clean_text, advanced_preprocessing
+from services.chunk_service import split_sentences
+from services.chunk_service import extract_propositions
 
 app = Flask(__name__)
 
@@ -15,7 +17,9 @@ def upload_file():
             file.save(filename)
             cleaned_text = clean_text(filename)
             processed_text = advanced_preprocessing(cleaned_text)
-            return redirect(url_for('display', filename=filename, processed_text=processed_text))
+            sentences = split_sentences(processed_text)
+            propositions = extract_propositions(cleaned_text)
+            return redirect(url_for('display', filename=filename, sentences=sentences, propositions=propositions))
     return render_template('templates\\summary.html')
 
 if __name__ == '__main__':
---
+++ services/chunk_service.py
@@ -0,0 +1,12 @@
+import spacy
+
+def extract_propositions(text):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    propositions = []
+    for sent in doc.sents:
+        for token in sent:
+            if token.dep_ in ("ROOT", "conj"):
+                proposition = " ".join([token.text for token in token.subtree])
+                propositions.append(proposition)
+    return propositions
