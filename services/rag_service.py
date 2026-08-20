7576b0c33d76654e98249e56a13b6fddcb54ab49
MrudulTarade <149925407+MrudulTarade@users.noreply.github.com>
2026-08-20 14:22:24 +0200

    foundation/rag

--- services/rag_service.py
+++ services/rag_service.py
@@ -1,5 +1,10 @@
 import nltk
+from embedding_service import embedded_sentences
+from vectorization import create_index, save_index
 
-def generate_answer(question, document_id):
-    
-    return True
+def generate_answer(question):
+    question = question.lower()
+    embedded_question = embedded_sentences([question])[0]
+    vector_index = create_index(embedded_question)
+    save_index(vector_index, f"vector_store/question_vector/{question}.index")
+    return "Answer generated for the question: " + question
