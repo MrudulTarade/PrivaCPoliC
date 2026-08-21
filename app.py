778ae77651c40f4b7f2bdbc4603eef22d27f20f8
MrudulTarade <149925407+MrudulTarade@users.noreply.github.com>
2026-08-21 12:17:52 +0200

    foundation/rag
    Question Embedding and Vector store complete.

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 from routes.upload_routes import upload_bp
 from routes.summary_routes import summary_bp
+from routes.chat_routes import chat_bp
 import config
 
 
 app = Flask(__name__)
 app.register_blueprint(upload_bp)
 app.register_blueprint(summary_bp)
+app.register_blueprint(chat_bp)
 
 @app.route('/')
 def input():
