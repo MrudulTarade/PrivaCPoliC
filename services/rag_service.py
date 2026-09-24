4b692b7f3ff31b2a80e62a014065ba6e2677b17f
MrudulTarade <149925407+MrudulTarade@users.noreply.github.com>
2026-09-24 21:21:37 +0200

    foundation/llm

--- services/rag_service.py
+++ services/rag_service.py
@@ -1,9 +1,10 @@
-import re, faiss
+import re, faiss, pickle
 import numpy as np
 from werkzeug.utils import secure_filename
 from sentence_transformers import SentenceTransformer
 from services.embedding_service import embed_question
 from services.vectorization import create_index, save_index, load_index, search
+from services.prompt_service import create_answer
 
 def generate_answer(question, file):
     file = secure_filename(file.filename)
@@ -13,4 +14,13 @@ def generate_answer(question, file):
     vector_index = create_index(embedded_question)
     save_index(vector_index, f"vector_store/question_vector/{question}.index")
     index = load_index(f"vector_store/{file}.index")
-    return "Answer generated for the question: " + question
+    indices = index.search(embedded_question, 3)
+    with open(f"vector_store/{file}.pkl", "rb") as f:
+        propositions = pickle.load(f)
+    related_chunks = []
+    for i in indices:
+        if indices[i] == propositions[i]:
+            related_chunks.append(propositions[i])
+    context = " ".join(related_chunks)
+    answer = create_answer(question, context)
+
